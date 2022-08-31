fa2afaf5a727ccfbc227a6d5e63a029d39344601
Cesar Sampaio Gomes <cesar.gomes@mstech.com.br>
2022-08-31 11:56:09 -0300

    Project 101% structured

---
+++ src/test/java/org/example/WebSpring/WebSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.WebSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
